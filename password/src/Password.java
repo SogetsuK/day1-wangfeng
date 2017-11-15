20bf2865626e5ba27788ef22acce68ecd1b18471
SogetsuK <33680195+SogetsuK@users.noreply.github.com>
2017-11-15 21:47:31 +0900

    修改1

--- password/src/Password.java
+++ password/src/Password.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 public class Password {
 	public static void main(String[] args) {
-		System.out.println("WELCOME TO THE BANK OF MITCHELL.");
-		System.out.print("ENTER YOUR PIN: ");
-		Scanner s1 = new Scanner(System.in);
-		int num = s1.nextInt();
-		System.out.println("\r" + "PIN ACCEPTED.YOU NOW HAVE ACCESS TO YOUR ACCOUNT." + "\r");
-		again(num);
+		int i = 12345;
+		again(i);
 	}
 	static void again(int a) {
 		System.out.println("WELCOME TO THE BANK OF MITCHELL.");
